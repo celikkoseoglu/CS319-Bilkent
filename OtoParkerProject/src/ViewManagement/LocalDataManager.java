b590ea7c02d54c7699ca73571325c74e0c65b938
celikkoseoglu <celikkoseoglu@yahoo.com>
2016-12-28 14:45:26 +0300

    Build breaker bugfix

--- OtoParkerProject/src/ViewManagement/LocalDataManager.java
+++ OtoParkerProject/src/ViewManagement/LocalDataManager.java
@@ -84,7 +84,7 @@ public class LocalDataManager {
             fr.close();
             return strBuffer.toString();
         } catch (FileNotFoundException fNEF) {
-            if (textFile.equals("progress.txt") || textFile.equals("stats.txt") || textFile.equals("unlocks.txt"))
+            if (textFile.equals("levelProgress.txt") || textFile.equals("stats.txt") || textFile.equals("unlocks.txt"))
                 return readText(textFile, true);
             else
                 return null;
