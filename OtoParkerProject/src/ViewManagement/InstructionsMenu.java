829464fac3da719dbef4fd26bfffada4d78a1c18
hbeyan <huseyinbeyan@gmail.com>
2016-12-28 20:57:42 +0000

    instructions menu

--- OtoParkerProject/src/ViewManagement/InstructionsMenu.java
+++ OtoParkerProject/src/ViewManagement/InstructionsMenu.java
@@ -28,16 +28,10 @@ public class InstructionsMenu extends OtoParkerMenu {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        AttributedString controlsString = new AttributedString("Controls");
-        controlsString.addAttribute(TextAttribute.FONT, new Font("Arial", Font.BOLD, 30));
-        controlsString.addAttribute(TextAttribute.FOREGROUND, new Color(201, 103, 32));
+        ImageIcon icon = new ImageIcon("images/instructions.jpg");
+        Image image = icon.getImage();
+        g.drawImage(image, 0, 50, (int)800, (int)500, null);
 
-        AttributedString upgradesString = new AttributedString("Mechanics");
-        upgradesString.addAttribute(TextAttribute.FONT, new Font("Arial", Font.BOLD, 30));
-        upgradesString.addAttribute(TextAttribute.FOREGROUND, new Color(201, 103, 32));
-
-        g2d.drawString(controlsString.getIterator(), 250, 40);
-        g2d.drawString(upgradesString.getIterator(), 500, 40);
     }
 
     class ButtonListener implements ActionListener {
